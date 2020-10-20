411d5a725e564223d1914332c1e67b5938524a36
isspek <ipekbrs@gmail.com>
2020-10-20 22:14:20 +0200

    create NELA task

--- src/data_reader.py
+++ src/data_reader.py
@@ -1,9 +1,15 @@
 import pandas as pd
 from argparse import ArgumentParser
 from pathlib import Path
+from .logger import logger
 import sqlite3
+import random
 
 DATA_DIR = Path('data')
+FOLD = 5
+NELA_DIR = DATA_DIR / 'NELA'
+Path(NELA_DIR).mkdir(parents=True, exist_ok=True)
+NELA_FNAME = '{mode}_{fold}.tsv'
 
 
 def read_constraint_splits():
@@ -17,50 +23,122 @@ def read_constraint_splits():
     }
 
 
+def normalize_source(source: str):
+    source = source.lower()
+    source = source.strip()
+    source = source.replace(" ", "")
+    return source
+
+
 def process_nela(nela_2018_path: str, nela_2019_path: str):
     '''
-    NELA datasets have the following columns:
+    NELA 2018 has the following metadata:
         - date
         - source
         - name (title)
         - content
+
+    NELA 2019 has the following metadata:
+        - id
+        - date
+        - source
+        - title
+        - content
+        - author
+        - url
+        - published
+        - published_utc
+        - collection_utc
+
+    We use aggregated labels from NELA 2019: unreliable: 2.0,  mixed: 1.0,  and  reliable: 0.0
     '''
     nela_2018_dir = Path(nela_2018_path)
     nela_2018_cnx = sqlite3.connect(nela_2018_dir / 'articles.db')
     nela_2018 = pd.read_sql_query("SELECT * FROM articles", nela_2018_cnx)
-    print(nela_2018.columns)
+    nela_2018.rename(columns={"name": "title"}, inplace=True)
+    # normalize sources of nela 2018
+    nela_2018['source'] = nela_2018.source.apply(lambda x: normalize_source(x))
 
     nela_2019_dir = Path(nela_2019_path)
-    nela_2019_cnx = sqlite3.connect(nela_2019_dir / 'articles.db')
+    nela_2019_cnx = sqlite3.connect(nela_2019_dir / 'nela-eng-2019.db')
     nela_2019 = pd.read_sql_query("SELECT * FROM newsdata", nela_2019_cnx)
-    print(nela_2019.columns)
+
+    labels_path = nela_2019_dir / 'labels.csv'
+    labels = pd.read_csv(labels_path)
+
+    reliable_sources = labels[labels['aggregated_label'] == 0.0]['source'].unique()
+    mixed_sources = labels[labels['aggregated_label'] == 1.0]['source'].unique()
+    unreliable_sources = labels[labels['aggregated_label'] == 2.0]['source'].unique()
+    satire_sources = labels[labels['Open Sources, satire'] >= 1.0]['source'].unique()
+
+    reliable_nela_2019 = nela_2019[nela_2019.source.isin(reliable_sources)]
+    mixed_nela_2019 = nela_2019[nela_2019.source.isin(mixed_sources)]
+    unreliable_nela_2019 = nela_2019[nela_2019.source.isin(unreliable_sources)]
+    satire_nela_2019 = nela_2019[nela_2019.source.isin(satire_sources)]
+
+    reliable_nela_2018 = nela_2018[nela_2018.source.isin(reliable_sources)]
+    mixed_nela_2018 = nela_2018[nela_2018.source.isin(mixed_sources)]
+    unreliable_nela_2018 = nela_2018[nela_2018.source.isin(unreliable_sources)]
+    satire_nela_2018 = nela_2018[nela_2018.source.isin(satire_sources)]
+
+    reliable_merged = pd.concat([reliable_nela_2018, reliable_nela_2019])[['source', 'title', 'content']]
+    mixed_merged = pd.concat([mixed_nela_2018, mixed_nela_2019])[['source', 'title', 'content']]
+    unreliable_merged = pd.concat([unreliable_nela_2018, unreliable_nela_2019])[['source', 'title', 'content']]
+    satire_merged = pd.concat([satire_nela_2018, satire_nela_2019])[['source', 'title', 'content']]
+    reliable_merged = resample_sources(reliable_merged)
+    mixed_merged = resample_sources(mixed_merged)
+    unreliable_merged = resample_sources(unreliable_merged)
+    satire_merged = resample_sources(satire_merged)
+
+    # create 5-fold datasets
+    for i in range(FOLD):
+        test = []
+        train = []
+
+        _test, _train = split_by_source(reliable_merged, reliable_sources)
+        train.append(_train)
+        test.append(_test)
+
+        _test, _train = split_by_source(mixed_merged, mixed_sources)
+        train.append(_train)
+        test.append(_test)
+
+        _test, _train = split_by_source(unreliable_merged, unreliable_sources)
+        train.append(_train)
+        test.append(_test)
+
+        _test, _train = split_by_source(satire_merged, satire_sources)
+        train.append(_train)
+        test.append(_test)
+
+        train = pd.concat(train)
+        test = pd.concat(test)
+        train.to_csv(NELA_DIR / NELA_FNAME.format(mode='train', fold=i + 1), sep='\t', index=False)
+        test.to_csv(NELA_DIR / NELA_FNAME.format(mode='test', fold=i + 1), sep='\t', index=False)
+
+        logger.info(f'Info about the fold {i + 1}')
+        logger.info(f'# of train samples {len(train)}')
+        logger.info(f'# of test samples {len(test)}')
 
 
-    # labels_path = nela_2019_dir / 'labels.csv'
-    # labels = pd.read_csv(labels_path)
-    #
-    # # use only Open Sources labels
-    # labels = labels[['Unnamed: 0', 'Open Sources, reliable', 'Open Sources, fake',
-    #                  'Open Sources, unreliable', 'Open Sources, bias',
-    #                  'Open Sources, conspiracy', 'Open Sources, hate',
-    #                  'Open Sources, junksci', 'Open Sources, rumor', 'Open Sources, blog',
-    #                  'Open Sources, clickbait', 'Open Sources, political',
-    #                  'Open Sources, satire', 'Open Sources, state']]
-    #
-    # labels_dict = {}
-    # for _, row in labels.iterrows():
-    #     source = row['Unnamed: 0'].lower()
-    #     labels_dict[source] = []
-    #     if row['Open Sources, reliable'] >= 2.0:
-    #         labels_dict[source].append('reliable')
-    #     if row['Open Sources, fake'] >= 2.0:
-    #         labels_dict[source].append('fake')
-    #     if row['Open Sources, bias'] >= 2.0:
-    #         labels_dict[source].append('bias')
-    #     if row['Open Sources, conspiracy'] >= 2.0:
-    #         labels_dict[source].append('conspiracy')
-    #     if row['Open Sources, conspiracy'] >= 2.0:
-    #         labels_dict[source].append('conspiracy')
+def resample_sources(sources):
+    sources = sources.groupby('source').filter(lambda x: x['title'].count() > 9)
+    sources.reset_index(drop=True, inplace=True)  # reset index
+    sources = sources.groupby('source').apply(
+        lambda x: x.sample(n=100) if len(x) > 100 else x).reset_index(drop=True)
+    return sources
+
+
+def split_by_source(data, sources):
+    idx = round(0.8 * len(sources))
+    if idx == len(sources) and idx != 0:
+        idx = idx - 1
+
+    random.shuffle(sources)
+    train = data[data['source'].isin(sources[:idx])]
+    test = data[data['source'].isin(sources[idx:])]
+    assert train.source.unique() is not test.source.unique()
+    return test, train
 
 
 if __name__ == '__main__':
